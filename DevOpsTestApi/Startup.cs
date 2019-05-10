a28d92a031efea4c2f4a344d6b57aaad5e19f022
Umair Syed <s.umair.riaz@gmail.com>
2019-05-10 11:57:35 +1000

    test2

--- DevOpsTestApi/Startup.cs
+++ DevOpsTestApi/Startup.cs
@@ -29,6 +29,7 @@ namespace DevOpsTestApi
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
